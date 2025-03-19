f0cab69af1a67d6215be7fc6dc78e4ae46ab17ea
theEarthian <1999prithviraj1999@gmail.com>
2025-03-19 09:51:20 +0000

    std::vector completed

--- vectors.cpp
+++ vectors.cpp
@@ -96,5 +96,80 @@ int main() {
   std::cout << " || Size : " << nums.size()
             << " || Capacity: " << nums.capacity() << std::endl;
 
+  std::cout << "Adding back the 1st value: ";
+  vec.insert(vec.begin(), 10);
+  for (int i : vec) {
+    std::cout << i << " ";
+  }
+  std::cout << " || Size : " << nums.size()
+            << " || Capacity: " << nums.capacity() << std::endl;
+
+  std::cout << "Adding back the 3rd value: ";
+  vec.insert(vec.begin() + 2, 30);
+  for (int i : vec) {
+    std::cout << i << " ";
+  }
+  std::cout << " || Size : " << nums.size()
+            << " || Capacity: " << nums.capacity() << std::endl;
+
+  std::cout << "Is vector empty? " << vec.empty() << std::endl;
+
+  std::cout << "Clearing the vector .. ";
+  vec.clear();
+  for (int i : vec) {
+    std::cout << i << " ";
+  }
+  std::cout << " || Size : " << nums.size()
+            << " || Capacity: " << nums.capacity() << std::endl;
+
+  std::cout << "Is vector empty? " << vec.empty() << std::endl;
+
+  // Vector Iterators - vec.begin() and vec.end().
+  // vec.beging() points directly to the memory loc and so, returns the memory.
+  // vec.end() doesn't point to n-1 index, but the next value which can be a
+  // random value or zero or garbage value based on the compiler.
+  std::vector<int> temp{100, 200, 500, 400, 700, 800};
+  std::cout << "Before insertion: ";
+  for (int k : temp) {
+    std::cout << k << " ";
+  }
+  std::cout << "\nAfter inserting at the 3rd position: ";
+  vec.insert(temp.begin() + 2, 1000);
+  for (int i : temp) {
+    std::cout << i << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "Values with indices: " << "[0] = " << *(temp.begin());
+  for (int i = 1; i < temp.size(); i++) {
+    std::cout << ", [" << i << "] = " << *(temp.begin() + i);
+  }
+  std::cout << std::endl;
+
+  std::cout << "Forward Iterator looping .." << std::endl;
+  std::cout << "Loops from forward: " << std::endl;
+
+  // Explicit iterator declaration (Optional, but demonstrates the concept)
+  std::vector<int>::iterator itr;
+  std::cout << "Explicit iterator: ";
+  for (itr = temp.begin(); itr != temp.end(); itr++) {
+    std::cout << *itr << " ";
+  }
+  std::cout << std::endl;
+
+  // In-loop iterator declaration
+  std::cout << "In-loop iterator: ";
+  for (auto i = temp.begin(); i != temp.end(); i++) {
+    std::cout << *i << " ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "Backward Iterator looping .. " << std::endl;
+  std::cout << "Loops from backward: ";
+  for (auto it = temp.rbegin(); it != temp.rend(); it++) {
+    std::cout << *it << " ";
+  }
+  std::cout << std::endl;
+
   return 0;
 }
