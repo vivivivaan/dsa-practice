04440bf26647c8c51767a0755caa86fab223c373
theEarthian <1999prithviraj1999@gmail.com>
2025-03-19 20:59:29 +0000

    completed stack and queue.

---
+++ queue.c++
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+// FIFO - add from rear/back/end and remove from front/start/begining.
+
+int main() {
+
+  std::queue<int> q;
+
+  // push()
+  for (int i = 1; i <= 7; i++) {
+    q.push(i * 100);
+  }
+
+  // size()
+  std::cout << "Queue size: " << q.size() << std::endl;
+
+  std::cout << "Queue elements: ";
+  // empty(), pop()
+  while (!q.empty()) {
+    std::cout << q.front() << " ";
+    q.pop();
+  }
+  std::cout << std::endl;
+
+  // Remaining all methods are same as in stack. 
+  // emplace(), swap(), back() is to display last element.
+
+  return 0;
+}
--- stack.cpp
+++ stack.cpp
@@ -11,27 +11,33 @@ int main() {
   for (int i = 1; i <= 7; i++) {
     stack.push(i * 5);
   }
-  std::cout << "Stack size: " << stack.size() << " | Stack using push: ";
+
+  std::cout << "Stack size: " << stack.size() << std::endl;
+  std::cout << "Stack using push: ";
 
   // empty(), top(), pop()
   while (!stack.empty()) {
     std::cout << stack.top() << " ";
     stack.pop();
   }
+
   // size()
-  std::cout << " | Stack size now: " << stack.size() << std::endl;
+  std::cout << "\nStack size now: " << stack.size() << std::endl;
 
   // emplace()
   for (int j = 1; j <= 8; j++) {
     stack.emplace(j * 10);
   }
-  std::cout << "Stack size: " << stack.size() << " | Stack using emplace: ";
+
+  std::cout << "Stack size: " << stack.size() << std::endl;
+  std::cout << "Stack using emplace: ";
 
   while (!stack.empty()) {
     std::cout << stack.top() << " ";
     stack.pop();
   }
-  std::cout << " | Stack size now: " << stack.size() << std::endl;
+
+  std::cout << "\nStack size now: " << stack.size() << std::endl;
 
   // Swapping stacks.
   std::stack<int> s1, s2;
@@ -39,21 +45,23 @@ int main() {
   for (int i = 1; i <= 5; i++) {
     s2.push(i * 15);
   }
+
   std::cout << "Before swapping: s2 size: " << s2.size()
-            << " | s1 size: " << s1.size() << std::endl;
-  std::cout << "Swapping s1 and s2 ..." << std::endl;
+            << " , s1 size: " << s1.size() << std::endl;
 
   s1.swap(s2);
 
-  std::cout << "Swapped s1 and s2: " << "s2 size: " << s2.size()
-            << " | s1 size: " << s1.size() << std::endl;
+  std::cout << "After swapping s1 and s2: " << "s2 size: " << s2.size()
+            << " , s1 size: " << s1.size() << std::endl;
   std::cout << "Elements after swapping: ";
+
   while (!s1.empty()) {
     std::cout << s1.top() << " ";
     s1.pop();
   }
-  std::cout << " | s1 size now: " << s1.size()
-            << " | s2 size now: " << s2.size() << std::endl;
+
+  std::cout << "\ns1 size now: " << s1.size() << " , s2 size now: " << s2.size()
+            << std::endl;
 
   return 0;
 }
