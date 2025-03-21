9a1ae2d6af9eec760e23b23de76fe02de83398f1
theEarthian <1999prithviraj1999@gmail.com>
2025-03-21 08:03:07 +0000

    std::multimap completed

---
+++ multimap.cpp
@@ -0,0 +1,31 @@
+// Same as map but can have duplicate keys.
+// [] can't be used, insert() or emplace() is used to add data.
+
+#include <iostream>
+#include <map>
+
+int main() {
+  std::multimap<std::string, int> mmap;
+
+  mmap.insert({"age", 25});
+  mmap.emplace("height", 177);
+  mmap.insert({"weight", 70});
+  mmap.emplace("yob", 1999);
+  mmap.insert({"bmi", 22});
+  mmap.insert({"age", 25});
+  mmap.emplace("height", 177);
+  mmap.insert({"weight", 70});
+  mmap.emplace("yob", 1999);
+  mmap.insert({"bmi", 22});
+
+  for (auto &x : mmap) {
+    std::cout << x.first << " : " << x.second << std::endl;
+  }
+
+  mmap.erase("bmi");
+  std::cout << "\nErasing BMI data..\n" << std::endl;
+
+  for (auto &x : mmap) {
+    std::cout << x.first << " : " << x.second << std::endl;
+  }
+}
