02bcc6b5dd450420f1f5dcd0d317b2e6be058265
theEarthian <1999prithviraj1999@gmail.com>
2025-03-28 10:37:47 +0000

    completed binary2decimal

--- C++_dsa_Udemy_LC_Problems/LinkedList/binary2decimal.cpp
+++ C++_dsa_Udemy_LC_Problems/LinkedList/binary2decimal.cpp
@@ -3,98 +3,101 @@
 using namespace std;
 
 class Node {
-    public:
-        int value;
-        Node* next;
-        Node(int value) {
-            this->value = value;
-            next = nullptr;
-        }
+public:
+  int value;
+  Node *next;
+  Node(int value) {
+    this->value = value;
+    next = nullptr;
+  }
 };
 
 class LinkedList {
-    private:
-        Node* head;
-        int length;
-        
-    public:
-        LinkedList(int value) {
-            Node* newNode = new Node(value);
-            head = newNode;
-            length = 1;
-        }
-        
-        ~LinkedList() {
-            Node* temp = head;
-            while (head) {
-                head = head->next;
-                delete temp;
-                temp = head;
-            }
-        }
+private:
+  Node *head;
+  int length;
 
-        void printList() {
-            Node* temp = head;
-            if (temp == nullptr) {
-                cout << "empty";
-            } else {
-                while (temp != nullptr) {
-                    cout << temp->value;
-                    temp = temp->next;
-                    if (temp != nullptr) {
-                        cout << " -> ";
-                    }
-                }
-            }
-            cout << endl;
-        }
+public:
+  LinkedList(int value) {
+    Node *newNode = new Node(value);
+    head = newNode;
+    length = 1;
+  }
 
-        Node* getHead() {
-            return head;
-        }
+  ~LinkedList() {
+    Node *temp = head;
+    while (head) {
+      head = head->next;
+      delete temp;
+      temp = head;
+    }
+  }
 
-        int getLength() {
-            return length;
+  void printList() {
+    Node *temp = head;
+    if (temp == nullptr) {
+      cout << "empty";
+    } else {
+      while (temp != nullptr) {
+        cout << temp->value;
+        temp = temp->next;
+        if (temp != nullptr) {
+          cout << " -> ";
         }
-        
-        void makeEmpty() {
-            Node* temp = head;
-            while (head) {
-                head = head->next;
-                delete temp;
-                temp = head;
-            }
-            length = 0;
-        }
+      }
+    }
+    cout << endl;
+  }
 
-        void append(int value) {
-            Node* newNode = new Node(value);
-            if (head == nullptr) {
-                head = newNode;
-            } else {
-                Node* currentNode = head;
-                while (currentNode->next != nullptr) {
-                    currentNode = currentNode->next;
-                }
-                currentNode->next = newNode;
-            }
-            length++;
-        }
+  Node *getHead() { return head; }
 
-        //   +======================================================+
-        //   |                 WRITE YOUR CODE HERE                 |
-        //   | Description:                                         |
-        //   | - Convert binary number in list to decimal           |
-        //   | - Return type: int                                   |
-        //   |                                                      |
-        //   | Tips:                                                |
-        //   | - Use a single pointer: 'current'                    |
-        //   | - Initialize an integer 'num' to 0                   |
-        //   | - Loop through the list                              |
-        //   | - Use the formula: num = num * 2 + current->value    |
-        //   | - Return 'num' as the decimal value                  |
-        //   +======================================================+
-        
+  int getLength() { return length; }
+
+  void makeEmpty() {
+    Node *temp = head;
+    while (head) {
+      head = head->next;
+      delete temp;
+      temp = head;
+    }
+    length = 0;
+  }
+
+  void append(int value) {
+    Node *newNode = new Node(value);
+    if (head == nullptr) {
+      head = newNode;
+    } else {
+      Node *currentNode = head;
+      while (currentNode->next != nullptr) {
+        currentNode = currentNode->next;
+      }
+      currentNode->next = newNode;
+    }
+    length++;
+  }
+
+  //   +======================================================+
+  //   |                 WRITE YOUR CODE HERE                 |
+  //   | Description:                                         |
+  //   | - Convert binary number in list to decimal           |
+  //   | - Return type: int                                   |
+  //   |                                                      |
+  //   | Tips:                                                |
+  //   | - Use a single pointer: 'current'                    |
+  //   | - Initialize an integer 'num' to 0                   |
+  //   | - Loop through the list                              |
+  //   | - Use the formula: num = num * 2 + current->value    |
+  //   | - Return 'num' as the decimal value                  |
+  //   +======================================================+
+
+  int binaryToDecimal() {
+    int num = 0;
+    Node *current = head;
+    while (current != nullptr) {
+      num = 2 * num + current->value;
+      current = current->next;
+    }
+    return num;
+  }
 };
-
-
