ddbe61ee55cdb1f67760156fdf43e971939560ca
theEarthian <1999prithviraj1999@gmail.com>
2025-03-30 02:04:32 +0100

    reorganized folder

--- From Scratch/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
- std::cout << "Deleted the head: " << cll->pop_head()->data << std::endl;
-//   std::cout << "The linkedlist is: ";
-//   cll->print_linkedlist();
-//   std::cout << std::endl;
-//   std::cout << "New length of the linkedlist is " << cll->length()
-//             << " and new head is " << cll->getHead()->data << std::endl
-//             << std::endl;
